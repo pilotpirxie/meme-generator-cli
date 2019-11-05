c0af01e574af9ee2d64fccad5dc28b290d7cf449
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2019-11-05 23:53:42 +0100

    First version of meme generator

---
+++ MemeGenerator/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Drawing;
+
+namespace MemeGenerator
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string inputImage = "";
+            string outputImage = "meme.jpg";
+            string upperText = "";
+            string bottomText = "";
+            string fontColor = "#ffffff";
+            int fontSize = 24;
+
+            try
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] == "--image" || args[i] == "-i")
+                    {
+                        if (args[i + 1].IndexOf("-") != 0)
+                        {
+                            inputImage = args[i + 1];
+                        }
+                    }
+
+                    if (args[i] == "--out" || args[i] == "-o")
+                    {
+                        if (args[i + 1].IndexOf("-") != 0)
+                        {
+                            outputImage = args[i + 1];
+                        }
+                    }
+
+                    if (args[i] == "--upper" || args[i] == "-u")
+                    {
+                        if (args[i + 1].IndexOf("-") != 0)
+                        {
+                            upperText = args[i + 1];
+                            upperText = upperText.Replace("\\n", Environment.NewLine);
+                        }
+                    }
+
+                    if (args[i] == "--bottom" || args[i] == "-b")
+                    {
+                        if (args[i + 1].IndexOf("-") != 0)
+                        {
+                            bottomText = args[i + 1];
+                            bottomText = bottomText.Replace("\\n", Environment.NewLine);
+                        }
+                    }
+
+                    if (args[i] == "--size" || args[i] == "-s")
+                    {
+                        if (args[i + 1].IndexOf("-") != 0)
+                        {
+                            fontSize = Convert.ToInt32(args[i + 1]);
+                        }
+                    }
+
+                    if (args[i] == "--color" || args[i] == "-c")
+                    {
+                        if (args[i + 1].IndexOf("-") != 0)
+                        {
+                            fontColor = args[i + 1];
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Wrong arguments {ex.ToString()}");
+                return -2;
+            }
+
+            if (inputImage == "")
+            {
+                Console.WriteLine("Not specified input image");
+                return -1;
+            }
+
+            if (upperText == "" && bottomText == "")
+            {
+                Console.WriteLine("Not specified text");
+                return -1;
+            }
+
+            Bitmap bitmap = new Bitmap(inputImage);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Font font = new Font("Impact", fontSize, FontStyle.Bold);
+            Rectangle rectangle = new Rectangle(0, fontSize / 2, bitmap.Width, bitmap.Height - fontSize/2);
+            StringFormat stringFormat = new StringFormat();
+            Color color = ColorTranslator.FromHtml(fontColor);
+
+            stringFormat.Alignment = StringAlignment.Center;
+            stringFormat.LineAlignment = StringAlignment.Near;
+            graphics.DrawString(upperText, font, new SolidBrush(color), rectangle, stringFormat);
+
+            stringFormat.Alignment = StringAlignment.Center;
+            stringFormat.LineAlignment = StringAlignment.Far;
+            graphics.DrawString(bottomText, font, new SolidBrush(color), rectangle, stringFormat);
+
+            bitmap.Save(outputImage);
+
+            return 0;
+        }
+    }
+}
